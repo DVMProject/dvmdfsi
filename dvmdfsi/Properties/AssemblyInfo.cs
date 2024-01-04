6814d11df1afc7680abb8d15552a758fdbbbd719
Bryan Biedenkapp <gatekeep@gmail.com>
2024-01-04 11:22:03 -0500

    bump build copyright date;

--- dvmdfsi/Properties/AssemblyInfo.cs
+++ dvmdfsi/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DVMProject Authors")]
 [assembly: AssemblyProduct("Digital Voice Modem DFSI")]
-[assembly: AssemblyCopyright("Copyright (c) 2023 Bryan Biedenkapp N2PLL and DVMProject Authors")]
+[assembly: AssemblyCopyright("Copyright (c) 2023-2024 Bryan Biedenkapp N2PLL and DVMProject Authors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
